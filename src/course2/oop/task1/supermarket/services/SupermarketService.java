011bd7cc2edf6ca13bb2c6fecc5b3d6493e3e155
Victor Valeev <viktorval2001@gmail.com>
2020-09-24 21:04:44 +0300

    began to implement services

--- src/course2/oop/task1/supermarket/services/SupermarketService.java
+++ src/course2/oop/task1/supermarket/services/SupermarketService.java
@@ -5,23 +5,60 @@ import course2.oop.task1.supermarket.Supermarket;
 import course2.oop.task1.supermarket.SupermarketPremise;
 import course2.oop.task1.utils.Pair;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SupermarketService {
 
-    public void fillSupermarketStorage(Supermarket m, Map<BaseProduct, Pair<Double, String>> prod) {
-        if (m.getStorage() == null) {
+    public void fillSupermarketStorage(Supermarket market, Map<BaseProduct, Pair<Double, String>> prods) {
+        if (market.getStorage() == null) {
             SupermarketPremise sp = new SupermarketPremise();
-            sp.setProducts(prod);
-            m.setStorage(sp);
-        } else if (m.getStorage().getProducts() == null) {
-            m.getStorage().setProducts(prod);
+            sp.setProducts(prods);
+            market.setStorage(sp);
+        } else if (market.getStorage().getProducts() == null) {
+            market.getStorage().setProducts(prods);
         } else {
-            addToStorage(m, prod);
+            addStorage(market, prods);
         }
     }
 
-    public void addToStorage(Supermarket m, Map<BaseProduct, Pair<Double, String>> prod) {
+    public void addStorage(Supermarket market, Map<BaseProduct, Pair<Double, String>> prods) {
+        if (market.getStorage() == null) {
+            fillSupermarketStorage(market, prods);
+        } else {
+            for (Map.Entry<BaseProduct, Pair<Double, String>> pc : prods.entrySet()) {
+                if (!market.getStorage().getProducts().containsKey(pc.getKey())) {
+                    market.getStorage().getProducts().put(pc.getKey(), pc.getValue());
+                } else {
+                    market.getStorage().getProducts().put(pc.getKey(), new Pair<Double, String>(market.getStorage().getProducts().get(pc.getKey()).getKey() + pc.getValue().getKey(), pc.getValue().getValue()));
+                }
+            }
+        }
+    }
 
+    public Map<BaseProduct, Pair<Double, String>> removeFromSupermarketStorage(Supermarket market, Map<BaseProduct, Pair<Double, String>> prods) {
+        Map<BaseProduct, Pair<Double, String>> removedProducts = new HashMap<>();
+
+        if (market.getStorage() != null) {
+            for (Map.Entry<BaseProduct, Pair<Double, String>> pc : prods.entrySet()) {
+                if (market.getStorage().getProducts().get(pc.getKey()).getKey() <= pc.getValue().getKey()) {
+                    removedProducts.put(pc.getKey(), market.getStorage().getProducts().remove(pc.getKey()));
+                } else {
+                    market.getStorage().getProducts().put(pc.getKey(), new Pair<Double, String>(market.getStorage().getProducts().get(pc.getKey()).getKey() - pc.getValue().getKey(), pc.getValue().getValue()));
+                    removedProducts.put(pc.getKey(), market.getStorage().getProducts().get(pc.getKey()));
+                }
+            }
+        }
+        return removedProducts;
+    }
+
+    public void MoveFromStorageToSupermarketHall(Supermarket market, Map<BaseProduct, Pair<Double, String>> prods) {
+        removeFromSupermarketStorage(market, prods);
+
+       if (market.getHall() == null) {
+           SupermarketPremise sp = new SupermarketPremise();
+           sp.setProducts(prods);
+           market.setHall(sp);
+       }
     }
 }
